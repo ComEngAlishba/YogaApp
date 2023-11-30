bcc8bafe3fb3878ed5bbceaa72765aa279ac89b1
Alishba Mirza <73831326+ComEngAlishba@users.noreply.github.com>
2023-11-30 12:50:29 +0000

    Hybrid App Initial Commit

---
+++ BookingYogaClass/BookingYogaClass/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingYogaClass
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
