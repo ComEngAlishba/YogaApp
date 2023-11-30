8a4634aef4af812e7d2959dac114730250c256fe
Alishba Mirza <73831326+ComEngAlishba@users.noreply.github.com>
2023-11-30 17:17:29 +0000

    Adding Classes - Customer, Course, ClassSchedule

---
+++ BookingYogaClass/BookingYogaClass/Data/ClassSchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingYogaClass.Data
+{
+    public class ClassSchedule
+    {
+        public int ScheduleId { get; set; }
+        public Course Course { get; set; }
+        public int CourseId { get; set; }
+        public string Date { get; set; }
+        public string TeacherName { get; set; }
+        public string AdditionalComments { get; set; }
+    }
+}
