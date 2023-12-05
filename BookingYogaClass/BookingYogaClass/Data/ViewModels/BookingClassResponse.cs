b06d05fc543c624c848847f2e5b633baac1536db
Alishba Mirza <73831326+ComEngAlishba@users.noreply.github.com>
2023-12-05 16:06:12 +0000

    Adding BookingClassResponse

---
+++ BookingYogaClass/BookingYogaClass/Data/ViewModels/BookingClassResponse.cs
@@ -0,0 +1,17 @@
+﻿using BookingYogaClass.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingYogaClass.Data.ViewModels
+{
+    public class BookingClassResponse
+    {
+        public int UploadEventMessag { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<ClassSchedule> ClassSchedules { get; set; } = new List<ClassSchedule>();
+    }
+}
